ca0a1b2ca2d6e9c85200aacdbe62a6c4446f9a26
Ian Campbell Brothers <iab15a@dmc-207-17.local>
2019-03-27 09:00:58 -0500

    Adding game over scenes

--- NinjaAttack/GameScene.swift
+++ NinjaAttack/GameScene.swift
@@ -117,7 +117,17 @@ class GameScene: SKScene {
     let actionMove = SKAction.move(to: CGPoint(x: -monster.size.width/2, y: actualY), duration: TimeInterval(actualDuration))
     
     let actionMoveDone = SKAction.removeFromParent()
-    monster.run(SKAction.sequence([actionMove, actionMoveDone]))
+    
+    let loseAction = SKAction.run() { [weak self] in
+      guard let `self` = self else { return }
+      let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+      let gameOverScene = GameOverScene(size: self.size, won: false)
+      self.view?.presentScene(gameOverScene, transition: reveal)
+    }
+    monster.run(SKAction.sequence([actionMove, loseAction, actionMoveDone]))
+    }
+
+
   }
   
   override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
